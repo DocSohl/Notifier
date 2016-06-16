33590a90fdfdc802553741528ccdf4eadcb5da4d
DocSohl <docsohl@gmail.com>
2016-06-16 00:23:49 -0700

    Setup core main loop and logging

--- Core/Notifier.py
+++ Core/Notifier.py
@@ -1,4 +1,28 @@
+import sys
+import traceback
+import logging
+
+class Notifier:
+    def __init__(self):
+        logging.basicConfig(filename="Logs/notifier.log")
+        self._logger = logging.getLogger("notifier")
+    
+    def run(self):
+        while 1:
+            try:
+                pass
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except Exception, e:
+                print traceback.format_exc()
+                self._logger.warn("Error in main loop", exc_info=True)
+            finally:
+                pass
+                
+
 
 
 if __name__=="__main__":
-    pass
+    app = Notifier()
+    app.run()
+    sys.exit()
